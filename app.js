f33996a76097d1a065b001b57b3cbabbf6f18e7b
tpmach <thi.p.mach@gmail.com>
2020-06-03 16:39:30 -0700

    Level 3-Hashing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 
 const app = express()
 const port = 3000
@@ -17,12 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true })
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
 
 const User = mongoose.model('User', userSchema)
 
@@ -41,7 +39,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
   newUser.save((err) => {
     if (!err) {
@@ -54,7 +52,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (!err) {
