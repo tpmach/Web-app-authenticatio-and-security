b7b9533346eec161ed9e94e6b36a526daa248ef8
Sylvia Mach <sylviamach@Thi.local>
2020-06-03 15:08:56 -0700

    Level 3 authentication

---
+++ app.js
@@ -0,0 +1,74 @@
+/* eslint no-undef: "error" */
+/* eslint-env node */
+
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const userSchema = new mongoose.Schema( {
+  email: String,
+  password: String
+})
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
+
+const User = mongoose.model('User', userSchema)
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  newUser.save((err) => {
+    if (!err) {
+      res.render('secrets')
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (!err) {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets')
+        }
+      }
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
