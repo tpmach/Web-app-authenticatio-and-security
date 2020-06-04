68c34672ee2d61f4cab3dcad98b233017611bd54
tpmach <thi.p.mach@gmail.com>
2020-06-03 18:24:01 -0700

    Level 4-Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const md5 = require('md5')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const app = express()
 const port = 3000
@@ -37,29 +38,39 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  })
-  newUser.save((err) => {
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
     if (!err) {
-      res.render('secrets')
-    } else {
-      console.log(err)
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      })
+      newUser.save((err) => {
+        if (!err) {
+          res.render('secrets')
+        } else {
+          console.log(err)
+        }
+      })
     }
   })
 })
 
 app.post('/login', (req, res) => {
   const username = req.body.username
-  const password = md5(req.body.password)
+  const password = req.body.password
+
+
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (!err) {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets')
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (!err) {
+            if (result) {
+              res.render('secrets')
+            }
+          }
+        })
       }
     } else {
       console.log(err)
