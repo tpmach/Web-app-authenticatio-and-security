77a67e1384327a6138e7baa205ac11f7a90cf9ce
tpmach <thi.p.mach@gmail.com>
2020-06-04 17:46:39 -0700

    Add submit route and show all the secrets anonymously

--- app.js
+++ app.js
@@ -91,11 +91,15 @@ app.get('/register', (req, res) => {
 })
 
 app.get('/secrets', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render('secrets')
-  } else {
-    res.redirect('/login')
-  }
+  User.find({ 'secret': { $ne: null } }, (err, foundUsers) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (foundUsers) {
+        res.render('secrets', { usersWithSecrets: foundUsers})
+      }
+    }
+  })
 })
 
 app.get('/submit', (req, res) => {
